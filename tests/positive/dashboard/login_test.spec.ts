8d8d2ce1c9ee77790326aa02a76b695c5e75af35
marioRamadhana <cemphes@gmail.com>
2024-07-09 14:43:01 +0700

    update the path directory

---
+++ tests/positive/dashboard/login_test.spec.ts
@@ -0,0 +1,27 @@
+import { test } from "@playwright/test";
+import { Constants } from "../../../utility/cosntant";
+import { Login } from "../../../pages/onBoarding/loginpage";
+
+test.beforeEach(async ({ page }) => {
+    await page.goto(Constants.baseUrl);
+})
+
+test('Login User', async ({ page }) => {
+    const login = new Login({ page });
+    await login.inputUsername('cemphes@gmail.com');
+    await login.inputPassword('Utiwmzx2');
+    await login.showPassword();
+    await login.clickLogin();
+
+})
+
+test('Forgot password', async ({ page }) => {
+    const login = new Login({ page });
+    await login.clickForgotPassword();
+})
+
+test('Login using phone number', async ({ page }) => {
+    const login = new Login({ page });
+
+    await login.clickLogInPhone();
+})
