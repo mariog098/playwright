dc950cc371744ae8bf3c769d64b4278368d2c746
marioRamadhana <cemphes@gmail.com>
2024-07-10 08:44:41 +0700

    add new variable

--- utility/cosntant.ts
+++ utility/cosntant.ts
@@ -1,3 +1,5 @@
 export class Constants {
     public static readonly baseUrl = 'https://shopee.com/';
+    public static readonly username = 'cemphes@gmail.com';
+    public static readonly password = 'Utiwmzx2'
 }
