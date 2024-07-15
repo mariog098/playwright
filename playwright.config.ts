cfc557a655c051b0a34a921f5cac17fc3e65944e
marioRamadhana <43749698+marioRamadhana@users.noreply.github.com>
2024-07-15 09:00:18 +0700

    Add Reporting module and Add negative scenario (#3)
    * add negative scenario for login
    
    * add new variable
    
    * add new setting for headless run, screenshot and video condition
    
    * add new module for allure reporting
    
    * ignore the allure directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -21,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['allure-playwright', { outputFolder: 'test-results' }],],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -29,6 +30,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
